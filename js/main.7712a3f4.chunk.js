d648e53b0a1da992261ecb7b4eb08ddd1d1b2df0
c1495616js <c1495616js@users.noreply.github.com>
2020-07-04 18:37:25 +0000

    deploy: 4abe80c69776e7edea265558dc8ee8c20a018a54

---
+++ js/main.7712a3f4.chunk.js
@@ -0,0 +1,49 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([["main"],{"2YZa":function(e,t,n){"use strict";var r=this&&this.__importDefault||function(e){return e&&e.__esModule?e:{default:e}};Object.defineProperty(t,"__esModule",{value:!0}),n("sg+I");const a=r(n("q1tI")),u=r(n("i8i4")),l=r(n("5D9J")),i=r(n("xKId")),o=l.default.div`
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;u.default.render(a.default.createElement((function(){return a.default.createElement(o,null,a.default.createElement("h1",null,"Simple Calendar"),a.default.createElement(i.default,null))}),null),document.getElementById("root"))},"PU8+":function(e,t,n){"use strict";var r=this&&this.__createBinding||(Object.create?function(e,t,n,r){void 0===r&&(r=n),Object.defineProperty(e,r,{enumerable:!0,get:function(){return t[n]}})}:function(e,t,n,r){void 0===r&&(r=n),e[r]=t[n]}),a=this&&this.__setModuleDefault||(Object.create?function(e,t){Object.defineProperty(e,"default",{enumerable:!0,value:t})}:function(e,t){e.default=t}),u=this&&this.__importStar||function(e){if(e&&e.__esModule)return e;var t={};if(null!=e)for(var n in e)Object.hasOwnProperty.call(e,n)&&r(t,e,n);return a(t,e),t},l=this&&this.__importDefault||function(e){return e&&e.__esModule?e:{default:e}};Object.defineProperty(t,"__esModule",{value:!0});const i=l(n("q1tI")),o=u(n("b/SL"));t.default=function({Day:e}){const t=o.startOfWeek(new Date,{weekStartsOn:1});return i.default.createElement(i.default.Fragment,null,Array(7).fill(null).map((n,r)=>{const a=o.format(o.addDays(t,r),"EEE");return i.default.createElement(e,{key:a},a)}))}},jrVH:function(e,t,n){"use strict";var r=this&&this.__createBinding||(Object.create?function(e,t,n,r){void 0===r&&(r=n),Object.defineProperty(e,r,{enumerable:!0,get:function(){return t[n]}})}:function(e,t,n,r){void 0===r&&(r=n),e[r]=t[n]}),a=this&&this.__setModuleDefault||(Object.create?function(e,t){Object.defineProperty(e,"default",{enumerable:!0,value:t})}:function(e,t){e.default=t}),u=this&&this.__importStar||function(e){if(e&&e.__esModule)return e;var t={};if(null!=e)for(var n in e)Object.hasOwnProperty.call(e,n)&&r(t,e,n);return a(t,e),t},l=this&&this.__importDefault||function(e){return e&&e.__esModule?e:{default:e}};Object.defineProperty(t,"__esModule",{value:!0});const i=l(n("q1tI")),o=l(n("5D9J")),f=u(n("b/SL")),d=o.default.div`
+  font-size: 18px;
+  font-weight: bold;
+  padding: 10px 10px 5px 10px;
+  display: flex;
+  justify-content: space-between;
+  background-color: #f5f6fa;
+`,c=o.default.div`
+  cursor: pointer;
+`;t.default=function({currentDate:e,setCurrentDate:t}){return i.default.createElement(i.default.Fragment,null,i.default.createElement(d,null,i.default.createElement(c,{onClick:()=>{t(f.subMonths(e,1))}},"Prev"),i.default.createElement("div",null,f.format(e,"MMMM yyyy")),i.default.createElement(c,{onClick:()=>{t(f.addMonths(e,1))}},"Next")))}},mAvD:function(e,t,n){"use strict";var r=this&&this.__createBinding||(Object.create?function(e,t,n,r){void 0===r&&(r=n),Object.defineProperty(e,r,{enumerable:!0,get:function(){return t[n]}})}:function(e,t,n,r){void 0===r&&(r=n),e[r]=t[n]}),a=this&&this.__setModuleDefault||(Object.create?function(e,t){Object.defineProperty(e,"default",{enumerable:!0,value:t})}:function(e,t){e.default=t}),u=this&&this.__importStar||function(e){if(e&&e.__esModule)return e;var t={};if(null!=e)for(var n in e)Object.hasOwnProperty.call(e,n)&&r(t,e,n);return a(t,e),t},l=this&&this.__importDefault||function(e){return e&&e.__esModule?e:{default:e}};Object.defineProperty(t,"__esModule",{value:!0});const i=l(n("q1tI")),o=u(n("b/SL")),f=l(n("5D9J")),d=f.default.div`
+  position: absolute;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+`,c=(e,t)=>o.format(e,"d M y")===o.format(t,"d M y");t.default=function({currentDate:e,Day:t,selectedDate:n,setSelectedDate:r}){const a=o.startOfMonth(e),u=o.endOfMonth(a),l=o.startOfWeek(a,{weekStartsOn:1}),f=o.endOfWeek(u,{weekStartsOn:1}),s=o.differenceInDays(f,l)+1,_=Array(s).fill(null).map((e,a)=>{const u=o.addDays(l,a),f=o.format(u,"d M"),s=o.format(u,"d");return i.default.createElement(t,{key:f,isToday:c(u,new Date),isSelected:c(u,n),onClick:()=>r(u)},i.default.createElement(d,null,s))});return i.default.createElement(i.default.Fragment,null,_)}},"sg+I":function(e,t,n){},xKId:function(e,t,n){"use strict";var r=this&&this.__createBinding||(Object.create?function(e,t,n,r){void 0===r&&(r=n),Object.defineProperty(e,r,{enumerable:!0,get:function(){return t[n]}})}:function(e,t,n,r){void 0===r&&(r=n),e[r]=t[n]}),a=this&&this.__setModuleDefault||(Object.create?function(e,t){Object.defineProperty(e,"default",{enumerable:!0,value:t})}:function(e,t){e.default=t}),u=this&&this.__importStar||function(e){if(e&&e.__esModule)return e;var t={};if(null!=e)for(var n in e)Object.hasOwnProperty.call(e,n)&&r(t,e,n);return a(t,e),t},l=this&&this.__importDefault||function(e){return e&&e.__esModule?e:{default:e}};Object.defineProperty(t,"__esModule",{value:!0});const i=u(n("q1tI")),o=l(n("5D9J")),f=n("qKvR"),d=l(n("jrVH")),c=l(n("PU8+")),s=l(n("mAvD")),_=o.default.div`
+  width: 400px;
+  border: 1px solid lightgrey;
+  box-shadow: 2px 2px 2px #eee;
+`,p=o.default.div`
+  width: 100%;
+  display: flex;
+  flex-wrap: wrap;
+`,m=o.default.div`
+  position: relative;
+  width: 14.2%;
+  padding-top: 14.2%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  cursor: pointer;
+  border-radius: 50%;
+
+  ${e=>e.isToday&&f.css`
+      border: 1px solid #eee;
+    `}
+
+  ${e=>e.isSelected&&f.css`
+      background-color: #eee;
+    `}
+`;t.default=function(){const[e,t]=i.useState(new Date),[n,r]=i.useState(new Date);return i.default.createElement(_,null,i.default.createElement(d.default,{currentDate:e,setCurrentDate:t}),i.default.createElement(p,null,i.default.createElement(c.default,{Day:m}),i.default.createElement(s.default,{currentDate:e,Day:m,selectedDate:n,setSelectedDate:r})))}}},[["2YZa","runtime","vendors"]]]);
